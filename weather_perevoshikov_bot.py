248b94879cfa06dc172216dc9b8e90090ef7355d
Perevoshikov89 <yuri.perevoshikov@gmail.com>
2024-12-04 17:02:00 +0400

    Первый черновик бота погоды

---
+++ weather_perevoshikov_bot.py
@@ -0,0 +1,28 @@
+import requests
+import telebot
+import json
+
+telegram_token = '7804132046:AAENrVW8Z5qwaJ7fFMWqo6nBsV-rLhLzFHI'
+weather_token = '5779cbdb104b00473fb6f0ab80598a6f'
+
+bot = telebot.TeleBot(token = telegram_token)
+
+
+@bot.message_handler(commands=["start"])
+def start_command(message):
+    bot.send_message(message.chat.id, "Привет! Напиши мне название города, и я пришлю сводку погоды.")
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}&units=metric')
+    data = json.loads(res.text)
+    bot.reply_to(message, f'Сейчас температура воздуха: {data ["main"]["temp"]} градусов, скорость ветра: {data ["wind"]["speed"]} м/с, влажность воздуха: {data ["main"]["humidity"]} %')
+
+
+
+
+
+
+
+bot.polling(non_stop = True)
